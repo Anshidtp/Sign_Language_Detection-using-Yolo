21b50933aa5728abc2225c63488412d8cffe647d
Anshidtp <shaanshid07@gmail.com>
2024-03-23 01:34:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguageDetection',
+    version= '0.0.0',
+    author= 'Anshid',
+    author_email= 'shaanshid07@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
